583ba41e8a24025ba3ed4ef971069d018b261cbd
blackcrw <blackzacky@protonmail.com>
2021-04-01 18:51:02 -0300

    Update main.cpp

--- 1 - Aula (Olá Mundo)/main.cpp
+++ 1 - Aula (Olá Mundo)/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+int main() {
+    std::cout << "Olá mundo!" << std::endl;
+
+    return 0;
+}
+
+/*
+#include <iostream>
+
 using namespace std;
 
 int main() {    
     cout << "Olá mundo!" << endl;
+
+    return 0;
 }
+*/
