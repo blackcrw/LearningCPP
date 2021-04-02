a24fadde87566cb33e366cf3f2752aa224e1e698
blackcrw <blackzacky@protonmail.com>
2021-04-02 10:00:57 -0300

    Create main.cpp

---
+++ 6 - Aula (Laço de Repetição For)/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    for(number = 0; number <= 100; number++) {
+        std::cout << "Laço de repetição: " << number << std::endl;
+    }
+
+    /*
+      for(int number = 0; number <= 100; number++) {
+          std::cout << "Laço de repetição: " << number << std::endl;
+      }
+    */
+
+    return 0;
+}
